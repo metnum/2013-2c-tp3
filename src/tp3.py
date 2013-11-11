21c8c7f172f9204e32842a69a8fc09e06c1a27b2
Andres Osinski <andres.osinski@gmail.com>
2013-11-11 06:42:39 -0300

    Working Python PageRank

--- src/tp3.py
+++ src/tp3.py
@@ -1,8 +1,9 @@
 #coding: utf-8
 # Implementación en Python del TP3 para prototipos y demas yerbas
 
-from numpy import matrix, any
+from numpy import matrix, any, zeros
 from numpy.linalg import norm, qr, solve
+from scipy.sparse import dok_matrix, lil_matrix
 import sys
 
 # Operaciones
@@ -38,65 +39,80 @@ import sys
 # Otras funciones matriz
 # sparse_matrix& identity(int m, int n)
 
-REMAIN_FACTOR = 0.95
+REMAIN_FACTOR = 0.85
 TELEPORTATION_FACTOR = 1 - REMAIN_FACTOR
 
 
 def load_data(raw_data):
-    pages = int(raw_data.pop(0))
-    links = int(raw_data.pop(0))
+    pages = int(raw_data.readline())
+    links = int(raw_data.readline())
 
-    graph = matrix(data=([[0.0] * pages] * pages))
+    #graph = zeros((pages, pages))
+    graph = lil_matrix((pages, pages), dtype='d')
 
-    for link in raw_data[0: links + 1]:
+    row_outdegrees = [0] * pages
+    count = 0
+    for link in raw_data:
         src, dest = [int(s) for s in link.split()]
         graph[src - 1, dest - 1] = 1
+        row_outdegrees[src - 1] += 1
+        count += 1
 
-    #normalize by the numbe of outdegrees
+        if count % 10000 == 0:
+            print "%s edges loaded..." % count
+
+
+    print "Normalizing out edges..."
     for rownum in range(graph.shape[0]):
-        if norm(graph[rownum, :]) != 0:
-            graph[rownum, :] = (graph[rownum, :] /
-                                sum([1 if graph[rownum, i] != 0 else 0 for
-                                     i in range(graph.shape[1])]))
+        if rownum % 10000 == 0:
+            print "%s pages normalized..." % rownum
+        if row_outdegrees[rownum] != 0:
+            for col in graph.rows[rownum]:
+                graph[rownum, col] = 1.0/row_outdegrees[rownum]
+        #else:
+        #    graph[rownum, :] = [1.0/pages] * pages
 
-    return graph
+    print "Matrix loaded. converting to CSR..."
+    graph = graph.tocsr()
+
+    return graph, row_outdegrees
 
 
-def has_no_out(web):
-    return matrix([[1 - float(any(row)) for row in web]]).T
+#def D(web):
+#    return matrix([[1 - float(any(row)) for row in web]]).T
 
-
-def v(web):
-    n = web.shape[0]
+def v(rows):
+    n = rows
     v = matrix([[1.0/ n]] * n)
     return v
 
 
-def build_uniform(web):
-    n, m = web.shape
-    d = has_no_out(web)
-    return d * v(web).T
+def D(web_outdegrees):
+    n = len(web_outdegrees)
+    d = matrix([[0 if row != 0 else 1 for row in web_outdegrees]]).T
+    return d * v(n).T
 
 
-def P_1(P, D):
-    return P + D
+def P_1(P, web_outdegrees):
+    return P + D(web_outdegrees)
 
 
-def P_2(P, v):
-    e = matrix([[1] * P.shape[0]])
+def P_2(P_1, v):
+    e = matrix([[1] * P_1.shape[0]])
     E = e.T * v.T
 
     c = REMAIN_FACTOR
-    P_2 = c * P + (1 - c) * E
+    P_2 = c * P_1 + (1 - c) * E
     return P_2
 
 
-def pagerank_power_kamvar(P, x, epsilon):
+def pagerank_power_kamvar(P, x, criteria, epsilon):
     """
     Perform the power method for the PageRank matrix
     using Kamvar's optimization for matrix multiplication.
 
-    P is the stochastic PageRank P' matrix
+    P is the non-stochastic PageRank P matrix
+    outdegrees is the vector with the count of outdegrees per
     x is the initial guess for the solution
     flow_factor 1 - TELEPORTATION_FACTOR
     """
@@ -107,15 +123,22 @@ def pagerank_power_kamvar(P, x, epsilon):
 
     n = len(x)
     v = matrix([ 1.0/n for i in range(n) ]).T
+    P_t = P.T
 
     while delta >= epsilon and k < MAX_ITERS:
         # Optimized Ax multiplication from algorithm 1
-        y = REMAIN_FACTOR * P.T * x
+        y = P_t.dot(REMAIN_FACTOR * x)
         w = norm(x, 1) - norm(y, 1)
         x_k = y + w * v
 
-        delta = norm(x_k - x, 1)
+        if criteria == 'abs':
+            delta = norm(x_k - x)
+        else:
+            delta = norm(x_k - x)/ norm(x)
+
+        print "Iter %s, delta=%s..." % (k, delta)
         k += 1
+        x = x_k
 
     return x_k
 
@@ -144,7 +167,7 @@ def quad_extrapolation(x_3, x_2, x_1, x_k):
     return x
 
 
-def power_quad(P, x, epsilon, quad_freq):
+def power_quad(P, x, criteria, epsilon, quad_freq):
     """
     Perform the power method for the PageRank matrix
     using Kamvar's optimization for matrix multiplication
@@ -156,25 +179,32 @@ def power_quad(P, x, epsilon, quad_freq):
     """
     MAX_ITERS = 100
 
-    k = 0
+    k = 1
     delta = 1000000.0  # At least one iteration will be performed
 
-    vec = v(web)
+    vec = v(web.shape[0])
 
     x_3 = x
     x_2 = x
     x_1 = x
     x_k = x
 
+    P_t = P.T
+
     while delta >= epsilon and k < MAX_ITERS:
         # Optimized Ax multiplication from algorithm 1
-        y = REMAIN_FACTOR * P.T * x_k
+        y = P_t.dot(REMAIN_FACTOR * x_k)
         w = norm(x_k, 1) - norm(y, 1)
         x_k = y + w * vec
 
         if k % quad_freq == 5:
+            print "Performing quad extrapolation..."
             x_k = quad_extrapolation(x_3, x_2, x_1, x_k)
-        delta = norm(x_k - x_1, 1)
+
+        if criteria == 'abs':
+            delta = norm(x_k - x_1)
+        else:
+            delta = norm(x_k - x_1)/ norm(x_1)
         k += 1
 
         x_3 = x_2
@@ -186,14 +216,22 @@ def power_quad(P, x, epsilon, quad_freq):
 
 if __name__ == '__main__':
     filename = sys.argv[1]
-    raw_data = open(filename, 'r').readlines()
+    raw_data = open(filename, 'r')
 
-    web = load_data(raw_data)
+    print "Loading data..."
+    web, out_degrees = load_data(raw_data)
 
+    #from ipdb import set_trace; set_trace()
     # Create P''
-    D = build_uniform(web)
-    P = P_1(web, D)
-    #P2 = P_2(P, v(web))
+    #dense = web.todense()
+    #P2 = P_2(P_1(dense, out_degrees), v(len(out_degrees)))
 
-    power_quad(P, v(web), 0.00001, 15)
+    print "Computing PageRank with regular Power Method..."
+    res = pagerank_power_kamvar(web, v(web.shape[0]), 'rel', 0.0001)
+    print res / norm(res, 2)
+    print "Computing PageRank with regular Power Quad..."
+    res = power_quad(web, v(web.shape[0]), 'rel', 0.0001,  6)
+    print res / norm(res, 2)
 
+    # Uncomment to test result
+    #print P2.T.dot(res) / norm(res, 2)
